0b13432f9f4828c366a3359e362eda7dbd4bbcad
Felipe Leite <lipi4shared@hotmail.com>
2024-03-07 01:02:36 -0300

    Configurando Nodemon.

--- main.js
+++ main.js
@@ -1,12 +1,16 @@
 const { app, BrowserWindow } = require('electron')
 
-let mainWindow
-
 app.on('ready', () => {
-	mainWindow = new BrowserWindow({
-		width: 1000,
-		height: 600,
-		resizable: false
+	const mainWindow = new BrowserWindow({
+		width: 1200,
+		height: 700,
+		resizable: false,
+		hasShadow: true,
+		opacity: 0.92,
+		backgroundColor: '#101828',
+		x: 80,
+		y: 22,
+		autoHideMenuBar: true
 	})
 
 	mainWindow.loadURL(`file://${__dirname}/index.html`)
